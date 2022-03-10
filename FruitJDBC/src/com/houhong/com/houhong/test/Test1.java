64c587ef662284d9abdab26d7fc5aa5a490aef5a
AV_Hulk <418419010@qq.com>
2022-03-10 23:43:43 +0800

    hot-fix commit

--- FruitJDBC/src/com/houhong/com/houhong/test/Test1.java
+++ FruitJDBC/src/com/houhong/com/houhong/test/Test1.java
@@ -35,6 +35,7 @@ public class Test1 {
         System.out.println("hello git!");
         System.out.println("hello git3");
         System.out.println("master git4");
+        System.out.println("hot-fix test");
 
 
         if (count > 0) {
